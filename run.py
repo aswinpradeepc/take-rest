718027517103f3c09b65cda20b5417c94ec639c5
aswinpradeepc <aswinpradeepc@gmail.com>
2022-11-25 00:27:53 +0530

    changes

--- run.py
+++ run.py
@@ -29,36 +29,34 @@ def download_facelandmark_model():
 download_facelandmark_model()
 
 def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
+
+
 	A = dist.euclidean(eye[1], eye[5])
 	B = dist.euclidean(eye[2], eye[4])
 
-	# compute the euclidean distance between the horizontal
-	# eye landmark (x, y)-coordinates
+
+
 	C = dist.euclidean(eye[0], eye[3])
 
-	# compute the eye aspect ratio
+
 	ear = (A + B) / (2.0 * C)
 
-	# return the eye aspect ratio
-	return ear
 
+	return ear
 
 
 EYE_AR_THRESH = 0.3
 EYE_AR_CONSEC_FRAMES = 3
 
-
 COUNTER = 0
 TOTAL = 0
-
 
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("model/shape_predictor_68_face_landmarks.dat")
 
-
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
@@ -66,7 +64,7 @@ predictor = dlib.shape_predictor("model/shape_predictor_68_face_landmarks.dat")
 cap = cv2.VideoCapture(0)
 t = dt.datetime.now()
 
-
+# loop over frames from the video stream
 MIN_WISE = {}
 while True:
 
@@ -94,6 +92,7 @@ while True:
 		shape = predictor(gray, rect)
 		shape = face_utils.shape_to_np(shape)
 
+
 		leftEye = shape[lStart:lEnd]
 		rightEye = shape[rStart:rEnd]
 		leftEAR = eye_aspect_ratio(leftEye)
@@ -102,8 +101,7 @@ while True:
 
 		ear = (leftEAR + rightEAR) / 2.0
 
-		# compute the convex hull for the left and right eye, then
-		# visualize each of the eyes
+
 		leftEyeHull = cv2.convexHull(leftEye)
 		rightEyeHull = cv2.convexHull(rightEye)
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
@@ -115,7 +113,7 @@ while True:
 
 
 		else:
-s
+
 			if COUNTER >= EYE_AR_CONSEC_FRAMES:
 				TOTAL += 1
 
@@ -127,14 +125,14 @@ s
 
 		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-		#cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-		#	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
-p
+
 	if key == ord("q"):
 
 		break
